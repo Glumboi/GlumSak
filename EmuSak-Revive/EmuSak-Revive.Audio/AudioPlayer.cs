a97b5fe2c14748d2017737faebbf007ebcd43351
Glumboi <58010838+Glumboi@users.noreply.github.com>
2022-11-24 00:52:11 +0100

    Source code update for 2.1.1

---
+++ EmuSak-Revive/EmuSak-Revive.Audio/AudioPlayer.cs
@@ -0,0 +1,61 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace EmuSak_Revive.Audio
+{
+    public class AudioPlayer
+    {
+        private WaveOut player = new WaveOut();
+
+        public float Volume
+        { get => player.Volume; set { player.Volume = value; } }
+
+        public List<string> AudioFiles { get; set; }
+
+        /// <summary>
+        /// Volume must be a value between 0 and 100
+        /// </summary>
+        /// <param name="volume"></param>
+        public AudioPlayer(int volume)
+        {
+            Volume = volume / 100f;
+        }
+
+        /// <summary>
+        /// Volume must be a value between 0 and 100
+        /// </summary>
+        /// <param name="volume"></param>
+        public void SetVolume(int volume)
+        {
+            Volume = volume / 100f;
+        }
+
+        /// <summary>
+        /// File must be a path!
+        /// For example: C:\yourfile.wav
+        /// It has to be WAV!
+        /// </summary>
+        /// <param name="fileName"></param>
+        public void PlayFile(string fileName)
+        {
+            try
+            {
+                var reader = new WaveFileReader(fileName);
+                player.Init(reader);
+                player.Play();
+            }
+            catch (Exception e)
+            {
+                //Do nothing
+                Debug.WriteLine("There was an error playing an audio file!\nDetailed error: " + e);
+            }
+        }
+
+        public void Stop()
+        {
+            player.Stop();
+        }
+    }
+}
---
+++ EmuSak-Revive/EmuSak-Revive.GUI/Generics/PlayAudio.cs
@@ -0,0 +1,20 @@
+﻿using EmuSak_Revive.Audio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmuSak_Revive.GUI.Generics
+{
+    internal class PlayAudio
+    {
+        public static void Play(AudioPlayer audioPlayer, bool playAudio, string file)
+        {
+            if (playAudio)
+            {
+                audioPlayer.PlayFile(file);
+            }
+        }
+    }
+}
